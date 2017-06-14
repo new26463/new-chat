17f43f058ae79ac2f50536987afdca6c738cda8b
new26463 <new26463@gmail.com>
2017-06-14 11:58:15 +0700

    qqq

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const sever = app.listen(3000)
+
+app.use(express.static('public'))
+
+app.set('views', path.join(__dirname, 'views'))
+  console.log('user eiei')
+  app.set('view engine', 'jade')
+
+app.get('/', (req, res)=> {
+  console.log('user eiei')
+  res.render('index')
+})
+
+app.get('/chat', (req, res)=>{
+  console.log('chat eiei');
+  res.render('chatRoom')
+})
+
+app.get('/admin', (req, res)=>{
+  consloe.log('admin eiei')
+  res.render('chatroom',{})
+})
+ //test
